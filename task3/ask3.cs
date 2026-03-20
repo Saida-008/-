0b5f31ef7df5bbf556b9d7826e4e0183b54fb9da
Saida-008 <s.tauman24@apec.edu.kz>
2026-03-20 12:13:05 +0500

    Add files via upload

---
+++ task3/ask3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+        int result = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number > 0)
+            {
+                result++;
+            }
+        }
+
+        Console.WriteLine("Number of elements greater than zero: " + result);
+    }
+}
